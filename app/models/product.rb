f2a79021603e5dc6018e7f9a9d6375d929aef8c2
Dmitriy Varvarovskiy <vardmev@gmail.com>
2016-06-10 16:23:54 +0300

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :title, length: { minimum: 10 }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'Url должен указывать на изображение формата GIF, JPG или PNG'
+  }
 end
