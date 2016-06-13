d4b590e89e821b03e22e7eee53502e2b1bc5abf7
Dmitriy Varvarovskiy <vardmev@gmail.com>
2016-06-13 11:38:33 +0300

    Catalog Display

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_actions', minimum: 3
   end
 
   test "should get new" do
