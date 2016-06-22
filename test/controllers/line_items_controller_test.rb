b9620bbb220c7181acd90041f12b4d3741e7e570
Dmitriy Varvarovskiy <vardmev@gmail.com>
2016-06-22 18:53:08 +0300

    Add an Order

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -26,12 +26,12 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item via ajax" do
     assert_difference('LineItem.count') do
-      xhr post :create, product_id: products(:ruby).id
+      xhr :post, :create, product_id: products(:ruby).id
     end
 
     assert_response :success
     assert_select_jquery :html, '#cart' do
-      asser_select 'tr#current_item td', /Programming Ruby 1.9/
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
     end
   end
 
